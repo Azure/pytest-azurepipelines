d1c926ba176923af23b0d0ac5049a18adc39a0ab
Anthony Shaw <anthonyshaw@apache.org>
2019-03-04 10:47:52 +0900

    try fixing the test output path on configuration too

--- pytest_azurepipelines.py
+++ pytest_azurepipelines.py
@@ -52,7 +52,7 @@ def pytest_configure(config):
     # ensure coverage creates xml format
     if config.pluginmanager.has_plugin('pytest_cov'):
         if 'xml' not in config.option.cov_report:
-            config.option.cov_report['xml'] = 'test-cov.xml'
+            config.option.cov_report['xml'] = os.path.normpath(os.path.abspath(os.path.expanduser(os.path.expandvars('test-cov.xml'))))
 
 
 def pytest_sessionfinish(session, exitstatus):
