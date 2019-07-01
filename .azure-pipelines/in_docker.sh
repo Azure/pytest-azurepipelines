1738499d1215ec852023f2659e2c2e314f4a48c2
Tim Gates <tim.gates@iress.com>
2019-07-01 21:57:30 +1000

    drop check

--- .azure-pipelines/in_docker.sh
+++ .azure-pipelines/in_docker.sh
@@ -5,9 +5,5 @@ set -euxo pipefail
 BASEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 TOP="$( dirname "${BASEDIR}" )"
 cd "${TOP}"
-if [ ! -e "${TOP}/pytest.ini" ] ; then
-    echo 'Configuration missing!' >&2
-    exit 1
-fi
 python2 -m pytest --cov=. --cov-report=xml -v -m "not testfail" -c "${TOP}/pytest.ini" tests
 python3 -m pytest --cov=. --cov-report=xml -v -m "not testfail" -c "${TOP}/pytest.ini" tests
