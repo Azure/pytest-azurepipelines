14ff7c1a71fc38c50ed70ac179c8a3c535dfb072
Tim Gates <tim.gates@iress.com>
2019-07-01 22:09:03 +1000

    Additional config option to clean up

--- .azure-pipelines/in_docker.sh
+++ .azure-pipelines/in_docker.sh
@@ -5,5 +5,5 @@ set -euxo pipefail
 BASEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 TOP="$( dirname "${BASEDIR}" )"
 cd "${TOP}"
-python2 -m pytest --cov=. --cov-report=xml -v -m "not testfail" -c "${TOP}/pytest.ini" tests
-python3 -m pytest --cov=. --cov-report=xml -v -m "not testfail" -c "${TOP}/pytest.ini" tests
+python2 -m pytest --cov=. --cov-report=xml -v -m "not testfail" tests
+python3 -m pytest --cov=. --cov-report=xml -v -m "not testfail" tests
