7ed7c8c65014286ef482a5e617c6e987cd682607
Anthony Shaw <anthony.p.shaw@gmail.com>
2019-08-30 11:06:36 +1000

    Use latest versions

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import os
 import codecs
 from setuptools import setup
 
-__version__ = "1.0.0.a1"
+__version__ = "1.0.0.a2"
 
 
 def read(fname):
@@ -33,7 +33,7 @@ setup(
     long_description=read('README.rst'),
     py_modules=['pytest_azurepipelines'],
     python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*',
-    install_requires=['pytest>=3.5.0', 'pytest-nunit==0.4.0'],
+    install_requires=['pytest>=3.5.0', 'pytest-nunit==0.5.0'],
 
     classifiers=[
 
